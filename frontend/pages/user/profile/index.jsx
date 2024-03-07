2b2a193409ebb94380eb2aa9fb7ae26ebab174d0
johnboris0414 <158587749+johnboris0414@users.noreply.github.com>
2024-03-07 14:29:22 +0900

    refactor : user profile update

--- frontend/pages/user/profile/index.jsx
+++ frontend/pages/user/profile/index.jsx
@@ -93,6 +93,12 @@ function MyProfile() {
                         <path d="M8.01 3H0V5H8.01V8L12 4L8.01 0V3Z" fill="#242731" />
                     </svg> */}
                 </button>
+                <button role="button" aria-label="Save" className="flex items-center justify-center py-4 px-7 focus:outline-none bg-white border rounded border-gray-400 mt-7 md:mt-14 hover:bg-gray-100  focus:ring-2 focus:ring-offset-2 focus:ring-gray-700">
+                    <span className="text-sm font-medium text-center text-gray-800 capitalize">Cancel</span>
+                    {/* <svg className="mt-1 ml-3" width={12} height={8} viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M8.01 3H0V5H8.01V8L12 4L8.01 0V3Z" fill="#242731" />
+                    </svg> */}
+                </button>
             </div>
             <style dangerouslySetInnerHTML={{ __html: "\n          .checkbox:checked + .check-icon {\n              display: flex;\n          }\n      " }} />
         </div>
